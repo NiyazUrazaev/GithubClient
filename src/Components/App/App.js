f0b2adfb09e8a9d03edfdc6cb8d9e2eaf15f6001
Niyaz <niyazga@icloud.com>
2019-11-05 22:29:56 +0300

    Create test layout

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import Button from '@material-ui/core/Button';
 
-const App = ({counter, name, increment, decrement, reset}) => (
+const App = ({counter, name, increment, decrement, reset, myProfile, myRepos, exit}) => (
   <div>
+    <div className='Header'>
+      <h1>
+        Github Client
+      </h1>
+      <Button variant="contained" color="primary" onClick={() => myProfile()}>
+        My profile
+      </Button>
+      <Button variant="contained" color="primary" onClick={() => myRepos()}>
+        My repositories
+      </Button>
+      <Button variant="contained" color="primary" onClick={() => exit()()}>
+        Exit
+      </Button>
+    </div>
     <div>{name}</div>
     <div>{counter}</div>
     <button onClick={() => increment()}>Increment</button>
